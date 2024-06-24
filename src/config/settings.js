a4e0cf69d4f06ca656d094901e51ee4a9ebc53e5
wildandr <wildandzakyramadhani@mail.com>
2024-06-24 23:36:24 +0700

    m

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,8 +1,8 @@
 export default {
 	title: `WildanZake Portfolio | Data Science | UI/UX | Backend Developer`,
 	description: `Data Science | UI/UX | Backend Developer`,
-	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
-	name: `Odyssey`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+	// url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
+	name: `Wildanzake`, // The short name of the business or brand name. Used for things like the copyright in the footer.
   enableThemeSwitcher: true,
   showLittleSticksPlug: true, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
 };
